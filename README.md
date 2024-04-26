b15a240657db27baa78daa324e3421076a25dbf1
Clint Strong <clintjstrong@gmail.com>
2024-04-25 23:12:30 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# workout-duration-calculator
+ An app made with react to check how long a workout would take with certain criteria set.
---
+++ src/App.js
@@ -0,0 +1,298 @@
+import { useState } from "react";
+
+export default function App() {
+  const [showAddWorkout, setShowAddWorkout] = useState(false);
+  const [addDay, setAddDay] = useState("Monday");
+  const [addRest, setAddRest] = useState("");
+  const [newWorkouts, setNewWorkouts] = useState([]);
+  const [workQuantity, setWorkQuantity] = useState(0);
+  const [setQuantity, setSetQuantity] = useState(0);
+  const [repQuantity, setRepQuantity] = useState(0);
+  const [showEditWorkout, setShowEditWorkout] = useState(null);
+
+  function handleFormSend(newWorkout) {
+    setNewWorkouts([...newWorkouts, newWorkout]);
+  }
+
+  function handleShowAddWorkout() {
+    setShowAddWorkout((showAddWorkout) => !showAddWorkout);
+    setShowEditWorkout(null);
+  }
+
+  function handleShowEditWorkout(theworkout) {
+    setShowEditWorkout(theworkout);
+    console.log(theworkout);
+    setShowAddWorkout(false);
+  }
+  return (
+    <div>
+      <Header
+        handleShowAddWorkout={handleShowAddWorkout}
+        showAddWorkout={showAddWorkout}
+      />
+      <WorkoutList
+        showAddWorkout={showAddWorkout}
+        newWorkouts={newWorkouts}
+        handleShowEditWorkout={handleShowEditWorkout}
+      />
+      {showAddWorkout && (
+        <AddWorkout
+          workQuantity={workQuantity}
+          setWorkQuantity={setWorkQuantity}
+          setQuantity={setQuantity}
+          setSetQuantity={setSetQuantity}
+          repQuantity={repQuantity}
+          setRepQuantity={setRepQuantity}
+          addDay={addDay}
+          setAddDay={setAddDay}
+          addRest={addRest}
+          setAddRest={setAddRest}
+          handleFormSend={handleFormSend}
+        />
+      )}
+      {showEditWorkout && (
+        <EditWorkout
+          workQuantity={workQuantity}
+          setWorkQuantity={setWorkQuantity}
+          setQuantity={setQuantity}
+          setSetQuantity={setSetQuantity}
+          repQuantity={repQuantity}
+          setRepQuantity={setRepQuantity}
+          addDay={addDay}
+          setAddDay={setAddDay}
+          addRest={addRest}
+          setAddRest={setAddRest}
+          showEditWorkout={showEditWorkout}
+        />
+      )}
+    </div>
+  );
+}
+
+function Header({ handleShowAddWorkout, showAddWorkout }) {
+  return (
+    <div className="header">
+      <h1>Workout Schedule and Timing</h1>
+      <button onClick={handleShowAddWorkout}>
+        {showAddWorkout ? "-" : "+"}
+      </button>
+    </div>
+  );
+}
+
+function WorkoutList({ newWorkouts, handleShowEditWorkout }) {
+  return (
+    <ul>
+      {newWorkouts.map((newWorkout) => (
+        <Workouts
+          newWorkout={newWorkout}
+          handleShowEditWorkout={handleShowEditWorkout}
+        />
+      ))}
+    </ul>
+  );
+}
+
+function Workouts({ newWorkout, handleShowEditWorkout }) {
+  return (
+    <div>
+      <li>
+        <span>{newWorkout.addDay} -</span> Workouts: {newWorkout.workQuantity},
+        Sets: {newWorkout.setQuantity}, Reps: {newWorkout.repQuantity},
+        Rest-Duration: {newWorkout.addRest},{" "}
+        <span>
+          Total Time:{" "}
+          {newWorkout.calculatedTime >= 1
+            ? `${newWorkout.calculatedTime} Hours`
+            : `${parseFloat(newWorkout.calculatedTime * 60).toFixed(
+                2
+              )} Minutes`}
+        </span>
+        <button
+          onClick={handleShowEditWorkout(newWorkout)}
+          className="pen-button"
+        >
+          🖊
+        </button>
+      </li>
+    </div>
+  );
+}
+
+function AddWorkout({
+  addRest,
+  addDay,
+  setAddDay,
+  setAddRest,
+  handleFormSend,
+  setRepQuantity,
+  repQuantity,
+  setSetQuantity,
+  setQuantity,
+  setWorkQuantity,
+  workQuantity,
+}) {
+  function handleSubmitForm(e) {
+    e.preventDefault();
+    if (!setQuantity || !repQuantity || !workQuantity || !addRest) return;
+    const calculatedTime = parseFloat(
+      (setQuantity * repQuantity * workQuantity * 3 +
+        workQuantity * setQuantity * addRest) /
+        3600
+    ).toFixed(2);
+    const newWorkout = {
+      addDay,
+      workQuantity,
+      setQuantity,
+      repQuantity,
+      addRest,
+      calculatedTime,
+    };
+    handleFormSend(newWorkout);
+    setWorkQuantity(0);
+    setRepQuantity(0);
+    setSetQuantity(0);
+    setAddRest("");
+    setAddDay("Monday");
+  }
+  return (
+    <div className="parentWorkouts">
+      <form className="addWorkouts" onSubmit={handleSubmitForm}>
+        <h1>Add a workout</h1>
+        <h3>
+          Day{" "}
+          <select
+            value={addDay}
+            placeholder="Select Day"
+            onChange={(e) => setAddDay(e.target.value)}
+          >
+            <option value="Monday">Monday</option>
+            <option value="Tuesday">Tuesday</option>
+            <option value="Wednesday">Wednesday</option>
+            <option value="Thursday">Thursday</option>
+            <option value="Friday">Friday</option>
+          </select>
+        </h3>
+        <h3>
+          Workouts{" "}
+          <select
+            value={workQuantity}
+            onChange={(workE) => setWorkQuantity(Number(workE.target.value))}
+          >
+            {Array.from({ length: 11 }, (_, i) => i).map((e) => (
+              <option value={e}>{e}</option>
+            ))}
+          </select>
+        </h3>
+        <h3>
+          Sets{" "}
+          <select
+            value={setQuantity}
+            onChange={(e) => setSetQuantity(Number(e.target.value))}
+          >
+            {Array.from({ length: 11 }, (_, i) => i).map((e) => (
+              <option value={e}>{e}</option>
+            ))}
+          </select>
+        </h3>
+        <h3>
+          Reps{" "}
+          <select
+            value={repQuantity}
+            onChange={(e) => setRepQuantity(Number(e.target.value))}
+          >
+            {Array.from({ length: 21 }, (_, i) => i).map((e) => (
+              <option value={e}>{e}</option>
+            ))}
+          </select>
+        </h3>
+        <h3>
+          Rest Duration{" "}
+          <input
+            value={addRest}
+            placeholder="In seconds whats your rest time?"
+            type="number"
+            onChange={(e) => setAddRest(Number(e.target.value))}
+          ></input>
+        </h3>
+        <button>Add</button>
+      </form>
+    </div>
+  );
+}
+function EditWorkout({
+  addRest,
+  addDay,
+  setAddDay,
+  setAddRest,
+  setRepQuantity,
+  repQuantity,
+  setSetQuantity,
+  setQuantity,
+  setWorkQuantity,
+  workQuantity,
+  showEditWorkout,
+}) {
+  return (
+    <form className="editWorkouts">
+      <h1>Edit {showEditWorkout.addDay}'s workout</h1>
+      <h3>
+        Day{" "}
+        <select
+          value={addDay}
+          placeholder="Select Day"
+          onChange={(e) => setAddDay(e.target.value)}
+        >
+          <option value="Monday">Monday</option>
+          <option value="Tuesday">Tuesday</option>
+          <option value="Wednesday">Wednesday</option>
+          <option value="Thursday">Thursday</option>
+          <option value="Friday">Friday</option>
+        </select>
+      </h3>
+      <h3>
+        Workouts{" "}
+        <select
+          value={workQuantity}
+          onChange={(workE) => setWorkQuantity(Number(workE.target.value))}
+        >
+          {Array.from({ length: 11 }, (_, i) => i).map((e) => (
+            <option value={e}>{e}</option>
+          ))}
+        </select>
+      </h3>
+      <h3>
+        Sets{" "}
+        <select
+          value={setQuantity}
+          onChange={(e) => setSetQuantity(Number(e.target.value))}
+        >
+          {Array.from({ length: 11 }, (_, i) => i).map((e) => (
+            <option value={e}>{e}</option>
+          ))}
+        </select>
+      </h3>
+      <h3>
+        Reps{" "}
+        <select
+          value={repQuantity}
+          onChange={(e) => setRepQuantity(Number(e.target.value))}
+        >
+          {Array.from({ length: 21 }, (_, i) => i).map((e) => (
+            <option value={e}>{e}</option>
+          ))}
+        </select>
+      </h3>
+      <h3>
+        Rest Duration{" "}
+        <input
+          value={addRest}
+          placeholder="In seconds whats your rest time?"
+          type="number"
+          onChange={(e) => setAddRest(Number(e.target.value))}
+        ></input>
+      </h3>
+      <button className="change">Submit</button>
+    </form>
+  );
+}
